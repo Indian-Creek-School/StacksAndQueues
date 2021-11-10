e9bd5b56727251103752b956d0c6d4371b0be7b0
Malcolm Morris-Berlin <73406050+MalcolmMorrisBerlin@users.noreply.github.com>
2021-11-10 08:19:28 -0800

    setup maze constructor for different length mazes

--- Maze.java
+++ Maze.java
@@ -1,6 +1,17 @@
 public class Maze {
     //1-10 value for a long or short maze
     private int length;
-    
+    private String[][] m;
+    public Maze(int l){
+        length=l;
+        for(int i=0;i<10;i++){
+            if(i==length){
+                m=new String[length+5][length+5];
+            }
+        }
+    }
+    public void legalMove(){
+        
+    }
     
 }
