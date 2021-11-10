9ffc91a1b7bdf16a1aeacad92d3a7e3d0798d270
Malcolm Morris-Berlin <73406050+MalcolmMorrisBerlin@users.noreply.github.com>
2021-11-09 20:19:12 -0500

    stack testing

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 import java.util.Stack;
 import java.util.Queue;
 public class Main{
-
     public static void main(String args[]){
-
+        Stack<Integer> st1=new Stack<Integer>();
+        for(int i=1;i<=10;i++){
+            st1.push(i);
+        }
+        for(int i=1;i<=10;i++){
+            System.out.println(st1.pop());
+        }        
+        
     }
 }
