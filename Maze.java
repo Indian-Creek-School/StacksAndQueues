f9d94936fdcee16708181cc413999819197d59ee
Malcolm Morris-Berlin <73406050+MalcolmMorrisBerlin@users.noreply.github.com>
2021-11-10 09:10:50 -0800

    starting to setup the maze writing algorithm
    the algorithm will start by setting a start and end point drawing a figurative line between them then manipulate that line so it fits into the grid of the maze then randomly assign walls that will not intrude on the the set path

--- Maze.java
+++ Maze.java
@@ -3,16 +3,22 @@ public class Maze {
     private int length;
     private String[][] m;
     private player p;
+    private int[] start;
+    private int[] end;
     public Maze(int l,player x){
         p=x;
         length=l;
+        start[1]=(int)Math.random()+(length+5);
+        start[0]=(int)Math.random()+(length+5);
+        end[0]=(int)Math.random()+(length+5);
+        end[1]=(int)Math.random()+(length+5);
         for(int i=0;i<10;i++){
             if(i==length){
                 m=new String[length+5][length+5];
             }
         }
-        for(int i=0;i<length;i++){
-            for(int j=0;j<length;j++){
+        for(int i=0;i<length+5;i++){
+            for(int j=0;j<length+5;j++){
             }
         }
     }
