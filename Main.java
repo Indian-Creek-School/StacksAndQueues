52f141b93789f51f34cabf766c805787f71f2c59
Malcolm Morris-Berlin <73406050+MalcolmMorrisBerlin@users.noreply.github.com>
2021-11-15 10:37:53 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,6 +15,19 @@ public class Main{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920,1080);
         frame.setVisible(true);
+        JButton b1=new JButton("1");    
+        JButton b2=new JButton("2");    
+        JButton b3=new JButton("3");    
+        JButton b4=new JButton("4");    
+        JButton b5=new JButton("5");    
+        JButton b6=new JButton("6");    
+        JButton b7=new JButton("7");    
+        JButton b8=new JButton("8");    
+        JButton b9=new JButton("9");
+        frame.add(b1); frame.add(b2); frame.add(b3);  
+        frame.add(b4); frame.add(b5); frame.add(b6);  
+        frame.add(b7); frame.add(b8); frame.add(b9); 
+        frame.setLayout(new GridLayout(3,3));
         Maze m1=new Maze(10);
             Stack<Integer> st1=new Stack<Integer>();
             for(int i=1;i<=10;i++){
