cb11b0f1fd5aa1389fef0c07791ddb59eb3c9008
Arrow338 <92538313+Arrow338@users.noreply.github.com>
2021-11-28 22:45:37 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,50 @@
-    import java.util.Stack;
-    import java.util.Arrays;
-    import javax.swing.*;
-    import java.awt.*;
+import java.util.Stack;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main{
-        /*
-            To Do:
-                Setup a GUI
-                Make sure that I can make the maze character move as an animation
-                setup the walls as GUI elements
-                maybe make a start and stop button or something
-                
-        */
+public class Main extends JPanel implements ActionListener{
+
+    private JFormattedTextField text = new JFormattedTextField();
+    private JButton undo = new JButton();
     public static void main(String args[]){
         // Maze m1=new Maze(10);
+        Main m = new Main();
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1920,1080);
+        frame.setPreferredSize(new Dimension(1080,1000));
+        frame.setLayout(new GridLayout(0,2));
+        frame.setContentPane(m);
         frame.setVisible(true);
+
+        m.undo.setPreferredSize(new Dimension(100,100));
+        m.undo.addActionListener(m);
+        m.undo.setActionCommand("undo");
+        m.undo.setText("Undo");
+        frame.add(m.undo);
+
+        m.text.setPreferredSize(new Dimension(500,500));
+        m.text.addActionListener(m);
+        m.text.setActionCommand("text");
+        frame.add(m.text);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Stack<String> words = new Stack<String>();
+        if(e.getActionCommand().equals("text")){
+            words.push(text.getText());
+        }
+        if(e.getActionCommand().equals("undo")){
+            // words.pop();
+            // text.setText(words.peek().toString());
+            System.out.println("test");
+        }
+    }
+}
+
         //frame.setLayout(new GridLayout(m1.length,m1.length));
         //     Stack<Integer> st1=new Stack<Integer>();
         //     for(int i=1;i<=10;i++){
@@ -30,5 +57,12 @@ public class Main{
         // System.out.println(Arrays.toString(m1.start));
         // System.out.println(Arrays.toString(m1.end));
         // System.out.println(m1.length);
-    }
-}
+
+                /*
+            To Do:
+                Setup a GUI
+                Make sure that I can make the maze character move as an animation
+                setup the walls as GUI elements
+                maybe make a start and stop button or something
+                
+        */
