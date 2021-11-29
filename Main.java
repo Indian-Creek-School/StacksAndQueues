d5ab87fe9261c0a2d863079cb0332453cc980a6b
Arrow338 <92538313+Arrow338@users.noreply.github.com>
2021-11-28 21:52:53 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,22 +13,22 @@ public class Main{
                 
         */
     public static void main(String args[]){
-        Maze m1=new Maze(10);
-        // JFrame frame = new JFrame("My First GUI");
-        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.setSize(1920,1080);
-        // frame.setVisible(true);
-        // frame.setLayout(new GridLayout(m1.length,m1.length));
-            Stack<Integer> st1=new Stack<Integer>();
-            for(int i=1;i<=10;i++){
-                st1.push(i);
-            }
-            for(int i=0;i<m1.length+1;i++){
-                System.out.println(Arrays.toString(m1.winPath.pop()));
-            }
-        System.out.println("::::::");
-        System.out.println(Arrays.toString(m1.start));
-        System.out.println(Arrays.toString(m1.end));
-        System.out.println(m1.length);
+        // Maze m1=new Maze(10);
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920,1080);
+        frame.setVisible(true);
+        //frame.setLayout(new GridLayout(m1.length,m1.length));
+        //     Stack<Integer> st1=new Stack<Integer>();
+        //     for(int i=1;i<=10;i++){
+        //         st1.push(i);
+        //     }
+        //     for(int i=0;i<m1.length+1;i++){
+        //         System.out.println(Arrays.toString(m1.winPath.pop()));
+        //     }
+        // System.out.println("::::::");
+        // System.out.println(Arrays.toString(m1.start));
+        // System.out.println(Arrays.toString(m1.end));
+        // System.out.println(m1.length);
     }
 }
